2638d7cd7d3628d732b5e13e2a35a123ba072815
Fatih Ka <xybydy@gmail.com>
2022-05-12 12:03:08 +0300

    refactor utube

--- plugins/misc/utube/methods.py
+++ plugins/misc/utube/methods.py
@@ -16,6 +16,8 @@ from ..upload import upload
 
 YTDL_PYMOD = os.environ.get("YOUTUBE_DL_PATH", "yt_dlp")
 
+ytdl = get_custom_import_re(utube.YTDL_PYMOD)
+
 LOGGER = userge.getLogger(__name__)
 
 @pool.run_in_thread
